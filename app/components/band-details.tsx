6edaa1435e7155724d0202cd623e179ab70b7dae
macamp0328 <macamp0328@gmail.com>
2024-04-19 12:53:20 -0500

    fix: adding null picture.band check

--- app/components/band-details.tsx
+++ app/components/band-details.tsx
@@ -5,16 +5,26 @@ import { getPictureDetails } from '../lib/actions';
 export default async function BandDetails(params: { bandSlug: string }) {
   const pictureDetails = await getPictureDetails(params.bandSlug);
 
+  if (!pictureDetails) {
+    return <p>Error loading band details.</p>; // Handle null or undefined details
+  }
+
   return (
     <div className=" bg-blue-200  text-center">
-      <h1 className="text-xl font-bold">{pictureDetails.band.name}</h1>
-      <p className="text-sm text-gray-500 dark:text-gray-400">
-        {pictureDetails.takenAt.toDateString()}
-      </p>
-      {pictureDetails.venue && (
-        <p className="text-sm text-gray-500 dark:text-gray-400">
-          {pictureDetails.venue.name}
-        </p>
+      {pictureDetails.band ? (
+        <div>
+          <h1 className="text-xl font-bold">{pictureDetails.band.name}</h1>
+          <p className="text-sm text-gray-500 dark:text-gray-400">
+            {pictureDetails.takenAt.toDateString()}
+          </p>
+          {pictureDetails.venue && (
+            <p className="text-sm text-gray-500 dark:text-gray-400">
+              {pictureDetails.venue.name}
+            </p>
+          )}
+        </div>
+      ) : (
+        <p>Ooops. No band details available.</p>
       )}
     </div>
   );
