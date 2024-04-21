1533f93516f90148a79df8ad37325eb2816075c2
macamp0328 <macamp0328@gmail.com>
2024-04-21 12:11:09 -0500

    feat: added vercel speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import type { Metadata } from 'next';
 
 import { cutiveMono } from './components/fonts';
@@ -30,6 +31,7 @@ export default function RootLayout({
 
           {children}
           <Analytics />
+          <SpeedInsights />
         </Providers>
       </body>
     </html>
