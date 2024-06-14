a1ada7cb4b3ab7c245a226414541fb0620b19f2a
macamp0328 <macamp0328@gmail.com>
2024-06-14 11:14:03 -0500

    fix: meta image will work this time

--- app/lib/metadata-utils.ts
+++ app/lib/metadata-utils.ts
@@ -7,29 +7,29 @@ export function generatePictureMetadata(
 ): Metadata {
   if (!pictureDetails) {
     return {
-      title: "Miles's SXSW 2024: A DIY Music Zine",
+      title: "Miles's SXSW: A DIY Zine",
       description:
-        'Experience SXSW 2024 through the lens of Miles, a music enthusiast on a quest to catch 50 live sets. This digital zine is a gritty, unfiltered tribute to the vibrant energy of live music.',
+        'Experience SXSW 2024 through the lens of Miles. This digital zine is a gritty, unfiltered tribute to the energy of live music.',
       openGraph: {
         type: 'website',
-        title: "Miles's SXSW 2024: A DIY Music Zine",
+        title: "Miles's SXSW: A DIY Zine",
         description:
-          'Dive into the raw, unpolished beauty of SXSW 2024. Captured with a small camera, this digital zine is a tribute to the pulsating rhythm of live music and the artists who create it.',
+          'Dive into the raw, unpolished beauty of SXSW 2024. Captured with a small camera, this digital zine is a tribute to the live music and the artists who create it.',
         images: [
           {
-            url: `${process.env.NEXT_PUBLIC_AWS_CLOUDFRONT_DOMAIN}/statics/start-page-miles.jpg`,
+            url: `${process.env.NEXT_PUBLIC_AWS_CLOUDFRONT_DOMAIN}/statics/opengraph-meta.jpg`,
             alt: "Cover image for Miles's SXSW 2024 Zine",
           },
         ],
       },
       twitter: {
         card: 'summary_large_image',
-        title: "Miles's SXSW 2024: A DIY Music Zine",
+        title: "Miles's SXSW: A Music Zine",
         description:
           'Join Miles on a journey through SXSW 2024. This digital zine, captured with a Ricoh GR II, is a raw, unfiltered celebration of live music and the artists who bring it to life.',
         images: [
           {
-            url: `${process.env.NEXT_PUBLIC_AWS_CLOUDFRONT_DOMAIN}/statics/start-page-miles.jpg`,
+            url: `${process.env.NEXT_PUBLIC_AWS_CLOUDFRONT_DOMAIN}/statics/opengraph-meta.jpg`,
             alt: "Cover image for Miles's SXSW 2024 Zine",
           },
         ],
