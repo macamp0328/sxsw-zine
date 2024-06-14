babb9bdfec809d408ba3b93c3871e95e065b7159
macamp0328 <macamp0328@gmail.com>
2024-06-14 09:33:33 -0500

    fix: meta on page

--- app/page.tsx
+++ app/page.tsx
@@ -3,37 +3,37 @@
 import { Zine } from './components/zine';
 import { fetchZinePictures } from './lib/actions';
 
-// export async function generateMetadata() {
-//   return {
-//     title: "Miles's SXSW 2024: A DIY Music Zine",
-//     description:
-//       "Experience SXSW 2024 through the lens of Miles, a music enthusiast on a quest to catch 50 live sets. This digital zine is a gritty, unfiltered tribute to the vibrant energy of live music.",
-//     openGraph: {
-//       type: "website",
-//       title: "Miles's SXSW 2024: A DIY Music Zine",
-//       description:
-//         "Dive into the raw, unpolished beauty of SXSW 2024. Captured with a small camera, this digital zine is a tribute to the pulsating rhythm of live music and the artists who create it.",
-//       images: [
-//         {
-//           url: "https://www.campmiles.com/photos/header-miles.jpg",
-//           alt: "Cover image for Miles's SXSW 2024 Zine",
-//         },
-//       ],
-//     },
-//     twitter: {
-//       card: "summary_large_image",
-//       title: "Miles's SXSW 2024: A DIY Music Zine",
-//       description:
-//         "Join Miles on a journey through SXSW 2024. This digital zine, captured with a Ricoh GR II, is a raw, unfiltered celebration of live music and the artists who bring it to life.",
-//       images: [
-//         {
-//           url: "https://www.campmiles.com/photos/header-miles.jpg",
-//           alt: "Cover image for Miles's SXSW 2024 Zine",
-//         },
-//       ],
-//     },
-//   };
-// }
+export async function generateMetadata() {
+  return {
+    title: "Miles's SXSW 2024: A DIY Music Zine",
+    description:
+      'Experience SXSW 2024 through the lens of Miles, a music enthusiast on a quest to catch 50 live sets. This digital zine is a gritty, unfiltered tribute to the vibrant energy of live music.',
+    openGraph: {
+      type: 'website',
+      title: "Miles's SXSW 2024: A DIY Music Zine",
+      description:
+        'Dive into the raw, unpolished beauty of SXSW 2024. Captured with a small camera, this digital zine is a tribute to the pulsating rhythm of live music and the artists who create it.',
+      images: [
+        {
+          url: `${process.env.NEXT_PUBLIC_AWS_CLOUDFRONT_DOMAIN}/statics/start-page-miles.jpg`,
+          alt: "Cover image for Miles's SXSW 2024 Zine",
+        },
+      ],
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: "Miles's SXSW 2024: A DIY Music Zine",
+      description:
+        'Join Miles on a journey through SXSW 2024. This digital zine, captured with a Ricoh GR II, is a raw, unfiltered celebration of live music and the artists who bring it to life.',
+      images: [
+        {
+          url: `${process.env.NEXT_PUBLIC_AWS_CLOUDFRONT_DOMAIN}/statics/start-page-miles.jpg`,
+          alt: "Cover image for Miles's SXSW 2024 Zine",
+        },
+      ],
+    },
+  };
+}
 
 export default async function Home() {
   const zinePictures = await fetchZinePictures();
