1c162b6b927a12b86c39aa7326a2da519e71f2ca
macamp0328 <macamp0328@gmail.com>
2024-07-29 11:22:25 -0500

    chore: removing speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import './globals.css';
 
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from '@vercel/speed-insights/next';
 import type { Metadata } from 'next';
 
 import HeaderLogo from '@/app/components/header-logo';
@@ -56,7 +55,6 @@ export default function RootLayout({
         <HeaderLogo />
         {children}
         <Analytics />
-        <SpeedInsights />
       </body>
     </html>
   );
